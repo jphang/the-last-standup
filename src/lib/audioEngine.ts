2540a6d51a5631e6e613fa2ab85fec48e8465eda
Justin Phang <justinjphang@gmail.com>
2026-07-31 16:33:27 +0100

    Updated audioEngine.ts

--- src/lib/audioEngine.ts
+++ src/lib/audioEngine.ts
@@ -115,9 +115,9 @@ function playLoop(
       if (timeoutId) clearTimeout(timeoutId);
       master.gain.cancelScheduledValues(ctx.currentTime);
       master.gain.setValueAtTime(0, ctx.currentTime);
-      activeOscs.forEach((o) => { try { o.stop(0); } catch(e) {console.debug("Caught exception")} });
+      activeOscs.forEach((o) => { try { o.stop(0); } catch(e) {console.debug("Caught exception", e)} });
       activeOscs.length = 0;
-      try { master.disconnect(); } catch(e) {console.debug("Caught exception")}
+      try { master.disconnect(); } catch(e) {console.debug("Caught exception", e)}
       activeTracks.delete(track);
     },
     isPlaying: () => !stopped,
